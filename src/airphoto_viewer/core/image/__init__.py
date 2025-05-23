1a1c546712577778d0532eab698242e43a076943
DreamHouse <dreamhouse@gmail.com>
2025-05-24 07:39:32 +0900

    Remove all Python code as part of C++ migration

--- src/airphoto_viewer/core/image/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-이미지 처리 기능을 제공하는 모듈입니다.
-
-이 모듈은 이미지 로딩, 변환, 처리 기능을 제공합니다.
-"""
-
-from .image_data import ImageData, ImageMetadata, load_image
-
-__all__ = ['ImageData', 'ImageMetadata', 'load_image']
--- src/airphoto_viewer/core/render/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-렌더링 기능을 제공하는 모듈입니다.
-
-이 모듈은 이미지 렌더링 및 표시 기능을 제공합니다.
-"""
-
-from .viewer_engine import ImageViewer, main as run_viewer
-
-__all__ = ['ImageViewer', 'run_viewer']
